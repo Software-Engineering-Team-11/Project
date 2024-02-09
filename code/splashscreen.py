7205216056a5e7cd91107ab8c5225b5c1d6c2fd7
rafaelrasse <rafaelrasse2@gmail.com>
2024-02-09 10:54:30 -0600

    fixed error in splash screen

--- code/splashscreen.py
+++ code/splashscreen.py
@@ -10,7 +10,7 @@ def build(root: tk.Tk) -> tk.Label:
     # Build the splash screen
     splash_screen: tk.Label = tk.Label(root, image=splash_image)
     splash_screen.place(x=0, y=0, relwidth=1, relheight=1)
-    splash_screen.image: ImageTk.PhotoImage = splash_image
+    splash_screen.image= splash_image
 
     # Make background black
     splash_screen.configure(background="black")

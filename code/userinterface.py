c854fed5f234c04fcfd7c6679dfbb5c518b6f6e9
Keller2 <46767006+Keller2@users.noreply.github.com>
2024-02-25 18:59:52 -0600

    Errors caused by merge with Rafael B code

--- code/userinterface.py
+++ code/userinterface.py
@@ -50,6 +50,51 @@ def on_continue_clicked(root: tk.Tk, users, input_ids) -> None:
     messagebox.showinfo("Success", "User information inserted successfully.")
 
 
+def builder(root:tk.Tk, users :dict) -> None:
+    builder: pygubu.Builder = pygubu.Builder()
+    try:
+        builder.add_from_file("ui/player_interface.ui")
+    except:
+        builder.add_from_file("../ui/player_interface.ui")
+    # Place the main frame in the center of the root window
+    # make unresizable
+    main_frame: tk.Frame = builder.get_object("master", root)
+    main_frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+    # Create frames for the teams
+    teams_frame: tk.Frame = builder.get_object("teams", main_frame)
+    red_frame: tk.Frame = builder.get_object("red_team", teams_frame)
+    blue_frame: tk.Frame = builder.get_object("blue_team", teams_frame)
+
+    # Create a dictionary of  IDs and corresponding entry field IDs
+    input_ids: Dict[int, str] = {}
+    fields: List[str] = {
+        "red_equipment_id_",
+        "red_user_id_",
+        "red_username_",
+        "blue_equipment_id_",
+        "blue_user_id_",
+        "blue_username_"
+    }
+
+    # Add each entry field ID to the dictionary of entry field IDs
+    for i in range(1, 16):
+        for field in fields:
+            input_ids[builder.get_object(f"{field}{i}", red_frame if "red" in field else blue_frame).winfo_id()] = f"{field}{i}"
+    
+    print(input_ids)
+
+    #  Place focus on the first entry field
+    # builder.get_object("blue_equipment_id_1", blue_frame).focus_set()
+
+    # Testing submit button
+    builder.get_object("submit").configure(command=lambda: on_continue_clicked(root,users,input_ids))
+
+    # data = supabase.table("users").select("*").execute()
+    # print(data)
+    
+
+
 def build_ui(root: tk.Tk, users: dict) -> None:
     global build_ui_instance  # Make sure to use the global instance
 

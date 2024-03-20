f6242b1e538b7561a5ed29087aced9f07807a012
rafaelrasse <rafaelrasse2@gmail.com>
2024-03-20 11:53:07 -0500

    default values

--- code/behind_the_scenes.py
+++ code/behind_the_scenes.py
@@ -1,8 +1,9 @@
 from typing import Dict, List
 from users import User
 
-points_per_tag: int = 10
-points_per_base_hit: int = 100
+# CONFIGURE LATER!
+points_per_tag: int = 0
+points_per_base_hit: int = 0
 
 class theGame:
     def __init__(self, users_raw: Dict[str, List[User]]) -> None:
@@ -20,12 +21,13 @@ class theGame:
         for user in self.blue_users:
             self.blue_user_equipment_ids.append(user.equipment_ID)
 
-        # Team scores, set to default of zero
+        # Team scores
+        # DEFAULT 0(?)
         self.red_team_score: int = 0
         self.blue_team_score: int = 0
 
     #     # If bases are scored
         self.red_base_scored: bool = False
-        self.green_base_scored: bool = False
-        self.game_event_list = []
+        self.blue_base_scored: bool = False
+        self.game_event_actions = []
     
